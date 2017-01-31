dc7a8b7a03895c72c33c3df7067f4960ad6fc39a
Odyseus <venusyadonis@gmail.com>
2017-01-31 15:32:16 -0300

    Popup Translator applet - Sixth prototype. - Removed the history.ui file and placed its content inside the appletHelper.py file. - Translatable strings untouched.

--- Applets/0dyseus@PopupTranslator/files/0dyseus@PopupTranslator/appletHelper.py
+++ Applets/0dyseus@PopupTranslator/files/0dyseus@PopupTranslator/appletHelper.py
@@ -136,6 +136,96 @@ langList = {
     "zu": "Zulu (G)"
 }
 
+HISTORY_UI = '''
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Generated with glade 3.16.1 -->
+<interface>
+  <requires lib="gtk+" version="3.10"/>
+  <object class="GtkWindow" id="main_window">
+    <property name="height_request">200</property>
+    <property name="visible">True</property>
+    <property name="can_focus">False</property>
+    <property name="modal">True</property>
+    <property name="window_position">center</property>
+    <property name="default_width">{default_width}</property>
+    <property name="default_height">{default_height}</property>
+    <child>
+      <object class="GtkVBox" id="vbox3">
+        <property name="width_request">475</property>
+        <property name="visible">True</property>
+        <property name="can_focus">False</property>
+        <property name="border_width">12</property>
+        <property name="spacing">12</property>
+        <child>
+          <object class="GtkScrolledWindow" id="scrolledwindow6">
+            <property name="visible">True</property>
+            <property name="can_focus">True</property>
+            <property name="shadow_type">in</property>
+            <child>
+              <object class="GtkTreeView" id="treeview_history">
+                <property name="visible">True</property>
+                <property name="can_focus">True</property>
+                <property name="reorderable">True</property>
+                <property name="rules_hint">True</property>
+                <property name="enable_search">False</property>
+                <child internal-child="selection">
+                  <object class="GtkTreeSelection" id="treeview-selection1"/>
+                </child>
+              </object>
+            </child>
+          </object>
+          <packing>
+            <property name="expand">True</property>
+            <property name="fill">True</property>
+            <property name="position">0</property>
+          </packing>
+        </child>
+        <child>
+          <object class="GtkHButtonBox" id="hbuttonbox2">
+            <property name="visible">True</property>
+            <property name="can_focus">False</property>
+            <property name="spacing">15</property>
+            <property name="layout_style">end</property>
+            <child>
+              <object class="GtkButton" id="button_reload">
+                <property name="visible">True</property>
+                <property name="can_focus">True</property>
+                <property name="receives_default">True</property>
+                <property name="use_stock">True</property>
+              </object>
+              <packing>
+                <property name="expand">False</property>
+                <property name="fill">False</property>
+                <property name="position">1</property>
+              </packing>
+            </child>
+            <child>
+              <object class="GtkButton" id="button_close">
+                <property name="label">gtk-close</property>
+                <property name="visible">True</property>
+                <property name="can_focus">True</property>
+                <property name="receives_default">True</property>
+                <property name="use_stock">True</property>
+              </object>
+              <packing>
+                <property name="expand">False</property>
+                <property name="fill">False</property>
+                <property name="position">1</property>
+              </packing>
+            </child>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="position">1</property>
+          </packing>
+        </child>
+      </object>
+    </child>
+  </object>
+</interface>
+'''
+
 translations = {}
 
 
@@ -206,25 +296,16 @@ class GoogleTranslator:
 class HistoryWindow():
 
     def __init__(self):
-        glade_file_path = os.path.join(appletDir, "assets", "history.ui")
         [initial_window_width, initial_window_height,
             width_to_trigger_word_wrap] = sys.argv[2].split(",")
 
-        # Path(gladefile).read_text() doesn't work on Python 3.4. ¬¬
-        # And the Python nonsense continues!!!! (/%$(/%$/$%/&$%))!!!!!!
-        try:
-            file_content = str(Path(glade_file_path).read_text())
-        except:
-            glade_file = open(glade_file_path)
-            file_content = str(glade_file.read())
-            glade_file.close()
-
-        # The following crap is done so I can set a custom width/height for the window. ¬¬
-        file_content = file_content.replace("<default_width>", initial_window_width)
-        file_content = file_content.replace("<default_height>", initial_window_height)
+        history_ui = HISTORY_UI.format(
+            default_width=initial_window_width,
+            default_height=initial_window_height
+        )
 
         builder = Gtk.Builder()
-        builder.add_from_string(file_content)
+        builder.add_from_string(history_ui)
         self.window = builder.get_object("main_window")
         self.window.set_icon_from_file(os.path.join(appletDir, "icon.png"))
         self.window.set_title(_("Popup Translator history"))
@@ -245,7 +326,7 @@ class HistoryWindow():
         column2.set_resizable(True)
 
         cr3 = Gtk.CellRendererText()
-        column3 = Gtk.TreeViewColumn(_("Language Pair"), cr3, markup=2)
+        column3 = Gtk.TreeViewColumn(_("Language pair"), cr3, markup=2)
         column3.set_sort_column_id(2)
         column3.set_resizable(True)
 
@@ -278,7 +359,8 @@ class HistoryWindow():
         reload_button.set_label(_("Reload"))
         reload_button.set_tooltip_text(_("Reload translation history"))
         reload_button.connect("clicked", self.populate)
-        img_path = os.path.join(appletDir, "icons", "popup-translator-document-open-recent-symbolic.svg")
+        img_path = os.path.join(
+            appletDir, "icons", "popup-translator-document-open-recent-symbolic.svg")
         img_file = Gio.File.new_for_path(img_path)
         img_file_icon = Gio.FileIcon.new(img_file)
         img = Gtk.Image.new_from_gicon(img_file_icon, Gtk.IconSize.BUTTON)
